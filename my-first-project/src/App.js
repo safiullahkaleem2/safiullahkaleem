f0d88accd94f01de4633820500cbee52db674a61
safiullahkaleem2 <safiullahkaleem867@gmail.com>
2023-01-21 12:44:06 -0800

    hi

---
+++ my-first-project/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.js";
+function Profile() {
+  return (
+   <div className=" profile-container">
+    <img 
+    width="100"
+    src="https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg"
+    />
+    <p>Mark Zuckerberg</p>
+    </div>
+     );
+}
+
+export default Profile;
